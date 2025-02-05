46ce988217e5b432beb85f616ef9d3aedbfdecd5
SabinaGarayeva <sgarayeva13@gmail.com>
2025-02-05 12:27:08 +0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+import connectDatabase from "./config/database.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandlerMiddleware";
+import { BadRequestException } from "./utils/appError";
+import { ErrorCodeEnum } from "./enums/error-code.enum";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+  })
+);
+
+app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get(
+  `/`,
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    throw new BadRequestException(
+      "This is a bad request",
+      ErrorCodeEnum.AUTH_INVALID_TOKEN
+    );
+    res.status(HTTPSTATUS.OK).json({
+      message: "Hello Subscribe to the channel & share",
+    });
+  })
+);
+
+app.use(errorHandler);
+
+app.listen(config.PORT, async () => {
+  console.log(`Server is running on port ${config.PORT} in ${config.NODE_ENV}`);
+  await connectDatabase();
+});
