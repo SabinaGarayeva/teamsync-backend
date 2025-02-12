628fa2bc434580f2f90665da56b8eb02955607a0
SabinaGarayeva <sgarayeva13@gmail.com>
2025-02-12 12:08:11 +0400

    Login and Logout endpoints

--- src/config/passport.config.ts
+++ src/config/passport.config.ts
@@ -1,10 +1,15 @@
 import { Request } from "express";
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as LocalStrategy } from "passport-local";
 import { config } from "./app.config";
 import { NotFoundException } from "../utils/appError";
 import { ProviderEnum } from "../enums/account-provider.enum";
-import { loginOrCreateAccountService } from "../services/auth.service";
+import {
+  loginOrCreateAccountService,
+  verifyUserService,
+} from "../services/auth.service";
+import { verify } from "crypto";
 
 passport.use(
   new GoogleStrategy(
@@ -38,6 +43,23 @@ passport.use(
   )
 );
 
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      session: true,
+    },
+    async (email, password, done) => {
+      try {
+        const user = await verifyUserService({ email, password });
+        return done(null, user);
+      } catch (error: any) {
+        return done(error, false, { message: error?.message });
+      }
+    }
+  )
+);
 
 passport.serializeUser((user: any, done) => {
   done(null, user);
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,7 +4,11 @@ import AccountModel from "../models/account.model";
 import WorkspaceModel from "../models/workspace.model";
 import { Roles } from "../enums/role.enum";
 import RoleModel from "../models/roles-permission.model";
-import { BadRequestException, NotFoundException } from "../utils/appError";
+import {
+  BadRequestException,
+  NotFoundException,
+  UnauthorizedException,
+} from "../utils/appError";
 import MemberModel from "../models/member.model";
 import { ProviderEnum } from "../enums/account-provider.enum";
 
@@ -146,4 +150,30 @@ export const registerUserSerice = async (body: {
     session.endSession();
     throw error;
   }
+};
+
+export const verifyUserService = async ({
+  email,
+  password,
+  provider = ProviderEnum.EMAIL,
+}: {
+  email: string;
+  password: string;
+  provider?: string;
+}) => {
+  const account = await AccountModel.findOne({ provider, providerId: email });
+  if (!account) {
+    throw new NotFoundException("Invalid email or password provided");
+  }
+  const user = await UserModel.findById(account.userId);
+
+  if (!user) {
+    throw new NotFoundException("User not found for the given account");
+  }
+
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    throw new UnauthorizedException("Invalid email or password");
+  }
+  return user.omitPassword()
 };
