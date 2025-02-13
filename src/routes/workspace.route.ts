b975cbe100154b3b2f0a9c01db9ae67cd7008708
SabinaGarayeva <sgarayeva13@gmail.com>
2025-02-13 13:58:42 +0400

    Workpace endpoints - get workspace by id

--- src/routes/workspace.route.ts
+++ src/routes/workspace.route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createWorkspaceController, getAllWorkspacesUserIsMemberController } from "../controllers/workspace.controller";
+import { createWorkspaceController, getAllWorkspacesUserIsMemberController, getWorkspaceByIdController } from "../controllers/workspace.controller";
 
 const workspaceRoutes = Router();
 
 workspaceRoutes.post("/create/new", createWorkspaceController);
-workspaceRoutes.post("all", getAllWorkspacesUserIsMemberController);
+
+workspaceRoutes.post("/all", getAllWorkspacesUserIsMemberController);
+workspaceRoutes.post("/:id", getWorkspaceByIdController);
+
 
 export default workspaceRoutes;
