1e6602143d6311ef4c753b469651e6e62082c7d7
SabinaGarayeva <sgarayeva13@gmail.com>
2025-02-17 13:47:13 +0400

    Workspace endpoints - update workspace

--- src/services/workspace.service.ts
+++ src/services/workspace.service.ts
@@ -156,3 +156,22 @@ export const changeMemberRoleService = async (
     member,
   };
 };
+
+export const updateWorkspaceByIdService = async (
+  workspaceId: string,
+  name: string,
+  description?: string
+) => {
+  const workspace = await WorkspaceModel.findById(workspaceId);
+  if (!workspace) {
+    throw new NotFoundException("Workspace not found");
+  }
+
+  // update workspace details
+  workspace.name = name || workspace.name;
+  workspace.description = description || workspace.description;
+  await workspace.save();
+  return {
+    workspace,
+  };
+};
