439f66763e0e1133eebf76dd780eff28969c413d
SabinaGarayeva <sgarayeva13@gmail.com>
2025-02-18 13:17:53 +0400

    Project Endpoints - Get All Projects in the Workspac

--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -19,3 +19,29 @@ export const createProjectService = async (
   await project.save();
   return { project };
 };
+
+export const getAllProjectsInWorkspaceService = async (
+  workspaceId: string,
+  pageSize: number,
+  pageNumber: number
+) => {
+  // 1. Finding all projects in the workspace
+
+  const totalCount = await ProjectModel.countDocuments({
+    workspace: workspaceId,
+  });
+
+  const skip = (pageNumber - 1) * pageSize;
+
+  const projects = await ProjectModel.find({
+    workspace: workspaceId,
+  })
+    .skip(skip)
+    .limit(pageSize)
+    .populate("createdBy", "_id name profilePicture -password")
+    .sort({ createdAt: -1 });
+
+  const totalPages = Math.ceil(totalCount / pageSize);
+
+  return {projects, totalCount, totalPages, skip}
+};
