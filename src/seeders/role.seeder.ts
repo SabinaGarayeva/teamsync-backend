1faffbf6831ac1f26e0f0fe0be948670423c9bec
SabinaGarayeva <sgarayeva13@gmail.com>
2025-02-24 19:11:14 +0400

    commit

--- src/seeders/role.seeder.ts
+++ src/seeders/role.seeder.ts
@@ -5,49 +5,31 @@ import RoleModel from "../models/roles-permission.model";
 import { RolePermissions } from "../utils/role-permission";
 
 const seedRoles = async () => {
-  console.log("Seeding roles started...");
+  console.log("🚀 Starting role seeding...");
 
   try {
     await connectDatabase();
-
-    const session = await mongoose.startSession();
-    session.startTransaction();
-
-    console.log("Clearing existing roles...");
-    await RoleModel.deleteMany({}, { session });
 
     for (const roleName in RolePermissions) {
       const role = roleName as keyof typeof RolePermissions;
       const permissions = RolePermissions[role];
 
-      // Check if the role already exists
-      const existingRole = await RoleModel.findOne({ name: role }).session(
-        session
-      );
+      const existingRole = await RoleModel.findOne({ name: role });
       if (!existingRole) {
-        const newRole = new RoleModel({
-          name: role,
-          permissions: permissions,
-        });
-        await newRole.save({ session });
-        console.log(`Role ${role} added with permissions.`);
+        await RoleModel.create({ name: role, permissions });
+        console.log(`✅ Role '${role}' added.`);
       } else {
-        console.log(`Role ${role} already exists.`);
+        console.log(`🔄 Role '${role}' already exists. Skipping...`);
       }
     }
 
-    await session.commitTransaction();
-    console.log("Transaction committed.");
-
-    session.endSession();
-    console.log("Session ended.");
-
-    console.log("Seeding completed successfully.");
+    console.log("🎉 Role seeding completed.");
+    process.exit(0);
   } catch (error) {
-    console.error("Error during seeding:", error);
+    console.error("❌ Error during seeding:", error);
+    process.exit(1);
   }
 };
 
-seedRoles().catch((error) =>
-  console.error("Error running seed script:", error)
-);
+seedRoles().catch((error) => console.error("❌ Error running seed script:", error));
+
